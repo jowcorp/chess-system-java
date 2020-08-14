e8f8db98496aa494420975ad2c6250e02f977cc4
jowred <32473283+jowred@users.noreply.github.com>
2020-08-13 23:45:26 -0300

    Bispo

---
+++ src/main/java/chess/pieces/Bishop.java
@@ -0,0 +1,67 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Bishop extends ChessPiece {
+
+	public Bishop(Board board, Color color) {
+		super(board, color);
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[this.getBoard().getRows()][this.getBoard().getColumns()];
+		Position p = new Position(0, 0);
+		
+		// NW
+		p.setValues(this.position.getRow() - 1, this.position.getColumn() - 1);
+		while (this.getBoard().positionExists(p) && !this.getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() - 1, p.getColumn() - 1);
+		} // Abaixo possibilita avançar na casa de uma peça adversária, capturando-a e se posicionando ali
+		if (this.getBoard().positionExists(p) && this.isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// NE
+		p.setValues(this.position.getRow() - 1, this.position.getColumn() + 1);
+		while (this.getBoard().positionExists(p) && !this.getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() - 1, p.getColumn() + 1);
+		} // Abaixo possibilita avançar na casa de uma peça adversária, capturando-a e se posicionando ali
+		if (this.getBoard().positionExists(p) && this.isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// SW
+		p.setValues(this.position.getRow() + 1, this.position.getColumn() - 1);
+		while (this.getBoard().positionExists(p) && !this.getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() + 1, p.getColumn() - 1);
+		} // Abaixo possibilita avançar na casa de uma peça adversária, capturando-a e se posicionando ali
+		if (this.getBoard().positionExists(p) && this.isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// SE
+		p.setValues(this.position.getRow() + 1, this.position.getColumn() + 1);
+		while (this.getBoard().positionExists(p) && !this.getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() + 1, p.getColumn() + 1);
+		} // Abaixo possibilita avançar na casa de uma peça adversária, capturando-a e se posicionando ali
+		if (this.getBoard().positionExists(p) && this.isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		return mat;
+	}
+	
+	@Override
+	public String toString() {
+		return "B";
+	}
+
+}
